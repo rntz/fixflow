4605d553d9976ff2ba4121a9df7070515d80bc07
Michael Arntzenius <daekharel@gmail.com>
2016-04-26 01:09:16 +0100

    temporary work

--- flow.ml
+++ flow.ml
@@ -1,104 +1,113 @@
 (* a weird sort of recursive dataflow system? *)
-module type FlowKey = sig
-    include Map.OrderedType
-    val to_string : t -> string (* for debugging *)
-end
-
-module type FLOW = sig
-    type key
+module type Applicative = sig
     type 'a exp
-
-    (* applicative structure *)
     val pure : 'a -> 'a exp
+    val map : ('a -> 'b) -> 'a exp -> 'b exp
     val ap : ('a -> 'b) exp -> 'a exp -> 'b exp
-    val map : ('a -> 'b) -> 'a exp -> 'b exp (* convenience *)
-
-    (* lazily computing a fixed-point map *)
-    val fix : (key -> 'v) (* bottom value for each key *)
-              -> ((key -> 'v exp) -> key -> 'v exp)
-              -> ('v -> string)
-              -> key -> 'v
 end
 
-module Flow(Key: FlowKey) : (FLOW with type key = Key.t) = struct
-    type key = Key.t
-    module S = Set.Make(Key)
-    module M = Map.Make(Key)
-    type keyset = S.t
+module type FlowGraph = sig
+    type node
+    val node_compare : node -> node -> int
+    val string_of_node : node -> string
+
+    type value
+    val value_equal : value -> value -> bool
+    val string_of_value : value -> string
+
+    val init: node -> value
+    module MakeStep(A: Applicative) : sig
+        val step: (node -> value A.exp) -> node -> value A.exp
+    end
+end
+
+module Flow(G: FlowGraph): sig val get: G.node -> G.value
+                               val clear: unit -> unit
+                           end =
+struct
+    open G
+
+    module Node = struct type t = node let compare = node_compare end
+    module S = Set.Make(Node)
+    module M = Map.Make(Node)
+    type nodeset = S.t
     type 'a map = 'a M.t
 
-    (* takes: frozen
-     * returns: (value, changed, visited) *)
-    type 'a result = 'a * bool * keyset
-    type 'a exp = keyset -> 'a result
+    module Exp = struct
+        (* an exp is a function
+         * it takes: frozen
+         * it returns: (value, changed, visited) *)
+        type 'a result = 'a * bool * nodeset
+        type 'a exp = nodeset -> 'a result
 
-    let pure x _ = (x, false, S.empty)
-    let ap f a frozen =
-      let (func, f_changed, f_visited) = f frozen in
-      (* note the sideways information passing: in addition to the nodes we see
-       * as frozen, `a' sees `f_visited' as frozen.
-       * in some sense this is an optimisation. *)
-      let (arg, a_changed, a_visited) = a (S.union frozen f_visited) in
-      (func arg,
-       f_changed || a_changed,
-       S.union f_visited a_visited)
-    let map f a = ap (pure f) a
+        let pure x _ = (x, false, S.empty)
+        let ap f a frozen =
+          let (func, f_changed, f_visited) = f frozen in
+          (* note the sideways information passing: in addition to the nodes we
+           * see as frozen, `a' sees `f_visited' as frozen. this is an
+           * optimisation. *)
+          let (arg, a_changed, a_visited) = a (S.union frozen f_visited) in
+          (func arg,
+           f_changed || a_changed,
+           S.union f_visited a_visited)
+        let map f a = ap (pure f) a
+    end                         (* module Exp *)
 
-    (* my god, what have I done *)
-    let fix (init: key -> 'v)
-            (func: (key -> 'v exp) -> key -> 'v exp)
-            (value_to_string: 'v -> string)
-        : key -> 'v =
-      let open Printf in
-      let cache: 'v map ref = ref M.empty in
-      let put k v = cache := M.add k v !cache in
-      let get k = try M.find k !cache
-                  with Not_found -> let v = init k in (put k v; v) in
+    module Step = MakeStep(Exp)
+    open Step
 
-      (* visit function *)
-      let rec visit key frozen : 'v result =
-        let cached_value = get key in
-        let () = printf "visiting %s, current value %s\n%!"
-                        (Key.to_string key) (value_to_string cached_value) in
-        if S.mem key frozen
-        (* NB: The only reason we return (S.singleton key) here is so that the
-         * check for (not (S.mem key visited)) below, in `loop', works
-         * properly. If we removed that we could return S.empty. *)
-        then let () = print_endline " frozen!" in
-             (cached_value, false, S.singleton key)
-        else
-          let () = print_endline " computing!" in
-          let frozen = S.add key frozen in
-          let recur () = func visit key frozen in
-          let rec loop old_value changed_so_far =
-            let () = printf "iterating %s\n%!" (Key.to_string key) in
-            let (new_value, changed, visited) = recur () in
-            let () = printf "iterated %s, old value %s, new value %s, changed %B\n%!"
-                            (Key.to_string key)
-                            (value_to_string old_value)
-                            (value_to_string new_value)
-                            changed in
-            let changed = if old_value = new_value
-                          then changed
-                          else (put key new_value; true) in
-            (* if nothing changed, we're done *)
-            if not changed
-            then (old_value, changed_so_far, S.add key visited)
-            (* if we didn't depend on ourselves, no need to iterate *)
-            else if not (S.mem key visited)
-            then (new_value, changed_so_far || changed, S.add key visited)
-            (* keep computing until we haven't changed anything. *)
-            else loop new_value true
-          in loop cached_value false in
-      (* end of visit function *)
+    (* ---------- DATAFLOW GRAPH IMPLEMENTATION ---------- *)
+    let cache: value map ref = ref M.empty
+    let finished = ref S.empty
+    let clear () = (cache := M.empty; finished := S.empty)
 
-      let finished = ref S.empty in
-      fun key -> let (value, _, visited) = visit key !finished in
-                 let () = finished := S.union !finished visited in
-                 value
+    let put k v = cache := M.add k v !cache
+    let get k = try M.find k !cache
+                with Not_found -> let v = init k
+                                  in (put k v; v)
+
+    let rec get node =        (* `get' isn't recursive, but visit/iterate are *)
+      let (value, _, visited) = visit node !finished in
+      let () = finished := S.union !finished visited in
+      value
+
+    and visit (node: node) (frozen: nodeset): value result =
+      let cached_value = get node in
+      let () = printf "visiting %s, current value %s\n%!"
+                      (string_of_node node) (string_of_value cached_value) in
+      if S.mem node frozen
+      then let () = print_endline " frozen!" in
+           (* We return (S.singleton node) here so that the check for
+            * (not (S.mem node visited)) below, in `loop', works. *)
+           (cached_value, false, S.singleton node)
+      else
+        let () = print_endline " computing!" in
+        iterate node (S.add node frozen) cached_value
+
+    and iterate (node: node) (frozen: nodeset): value -> value result =
+      let rec loop changed_so_far old_value =
+        let () = printf "iterating %s\n%!" (string_of_node node) in
+        let (new_value, changed, visited) = step visit node frozen in
+        let () = printf "iterated %s, old value %s, new value %s, changed %B\n%!"
+                        (string_of_node node)
+                        (string_of_value old_value)
+                        (string_of_value new_value)
+                        changed in
+        let changed = if old_value = new_value
+                      then changed
+                      else (put node new_value; true) in
+        (* if nothing changed, we're done *)
+        if not changed
+        then (old_value, changed_so_far, S.add node visited)
+               (* if we didn't depend on ourselves, no need to iterate *)
+        else if not (S.mem node visited)
+        then (new_value, changed_so_far || changed, S.add node visited)
+               (* keep computing until we haven't changed anything. *)
+        else loop true new_value
+      in loop false
+
 end
 
-
 (* instances *)
 module S = Flow(struct type t = string
                        let compare = String.compare
@@ -118,10 +127,10 @@ let odd n = not (even n)
 
 
 (* examples *)
-let func1 (self: int -> int I.exp) (x: int): int I.exp =
-  let prev = self x in
-  match x with
-  | 0 -> I.map (fun x -> if x < 2 then x + 1 else x) prev
-  | n -> I.map ((+) 1) (self (n-1))
-
-let ex1 = I.fix (const 0) func1 string_of_int
+(* let func1 (self: int -> int I.exp) (x: int): int I.exp =
+ *   let prev = self x in
+ *   match x with
+ *   | 0 -> I.map (fun x -> if x < 2 then x + 1 else x) prev
+ *   | n -> I.map ((+) 1) (self (n-1))
+ * 
+ * let ex1 = I.fix (const 0) func1 string_of_int *)
